b979c61945a199da7cb55864886f89a211a4a9ba
IFFranciscoME <if.francisco.me@gmail.com>
2024-06-28 14:49:40 -0600

    Working minimal example

--- examples/basic_trading.py
+++ examples/basic_trading.py
@@ -1,10 +1,12 @@
 
-# --- -------------------------------------------------------------------- -- #
-# --- -------------------------------------------------------------------- -- #
+# --- ------------------------------------------------------------------- --- #
+# --- ------------------------------------------------------------------- --- #
+# --- ------------------------------------------------------------------- --- #
 
 import os
 import sys
 import toml
+import numpy as np
 
 # -- Set working directories
 cwd_path = os.getcwd()
@@ -14,7 +16,10 @@ sys.path.insert(0, package_path)
 from traderman.forecasters.benchmarks import Randomizer
 from traderman.connectors import binance as BinanceSpot
 
-# --- Get params from Config.toml 
+BINANCE_API_KEY = os.environ["BINANCE_API_KEY"]
+BINANCE_SECRET_KEY = os.environ["BINANCE_SECRET_KEY"]
+
+# --- Get params from Config.toml --------------------------------------- --- #
 
 with open(cwd_path + "/examples/basic_config.toml", "r") as file:
     config_data = toml.load(file)
@@ -22,18 +27,29 @@ with open(cwd_path + "/examples/basic_config.toml", "r") as file:
 SET_SEED = config_data["model"]["signal"]["model_params"]["seed"]
 SET_CLASSES = config_data["model"]["signal"]["model_params"]["classes"]
 
-# -- 
+# --- ------------------------------------------------------------------- --- #
 
-ModelSignal = Randomizer(seed=SET_SEED)
-forecasted_signal = ModelSignal.predict(predict_type="class",
-                                        classes=SET_CLASSES)
-
-#ModelRisk = 1
+ModelSignal = Randomizer(seed=SET_SEED, model_type="classifier")
+forecasted_signal = ModelSignal.predict(classes=SET_CLASSES)
 
 print(f"forecasted class: {forecasted_signal}")
-#print(f"volume to trade: {trade_volume}")
 
-# -- Trade Layer
+# --- ------------------------------------------------------------------- --- #
 
-#BinanceSpot.new_order()
+ModelRisk = Randomizer(seed=SET_SEED, model_type="regressor")
+forecasted_volume = ModelRisk.predict(lower=0.0001, upper=0.0009)
+forecasted_volume = np.round(forecasted_volume, 4)
 
+print(f"volume to trade: {forecasted_volume}")
+
+# --- ------------------------------------------------------------------- --- #
+trade_params = {"symbol": "BTCUSDT",
+                "type": "MARKET",
+                "side": forecasted_signal,
+                "quantity": forecasted_volume[0],}
+
+n_order = BinanceSpot.new_order(in_params=trade_params,
+                      api_key=BINANCE_API_KEY,
+                      secret_key=BINANCE_SECRET_KEY)
+
+print(n_order)
